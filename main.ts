d4d4bc4c282efc2a6a265763f409f86eacb132da
Rawk Akani <lionelmbankeu@gmail.com>
2025-01-10 20:05:36 +0200

    support for path

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ import { parseArgs } from "jsr:@std/cli/parse-args";
 
 const args = parseArgs(Deno.args)
 
+Deno.env.set("APP_SOURCEDIR", args.sourceDir || Deno.cwd());
+
 Deno.serve({ port: args.port || 8000 },async (req: Request) => {
     
     const decoder = new TextDecoder("utf-8"); 
@@ -16,9 +18,9 @@ Deno.serve({ port: args.port || 8000 },async (req: Request) => {
         return serveFile(req, `.${pathname}`) 
     }
 
-    console.log(Deno.cwd())
+    console.log(Deno.env.get("APP_SOURCEDIR"))
 
-    const main_html = await Deno.readFile(`${Deno.cwd()}/pages/index.html`)
+    const main_html = await Deno.readFile(`${Deno.env.get("APP_SOURCEDIR") || Deno.cwd()}/pages/index.html`)
     return new Response(decoder.decode(main_html), {
           headers: {
             "content-type": "text/html",
